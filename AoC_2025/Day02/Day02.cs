2f00d22486aad783d5120d9a932960d0a814e63c
Puskas David <puskas.david92@gmail.com>
2025-12-05 07:43:32 +0100

    Day2 solution optimized

--- AoC_2025/Day02/Day02.cs
+++ AoC_2025/Day02/Day02.cs
@@ -44,10 +44,102 @@ namespace AoC_2025
 
         public static long Day02_Part1(Day02_Input input)
         {
+            //return Part1_BF_Regex(input);
+            return Optimized(input, false);
+
+
+        }
+
+        private static long Optimized(Day02_Input input,  bool isPart2)
+        {
+            // Optimized version without regex
+            long sum = 0;
+
+            // Make new range list with only ranges that are same digit long
+            var filteredRanges = new Day02_Input();
+            foreach (var (start, end) in input)
+            {
+                int startLen = start.ToString().Length;
+                int endLen = end.ToString().Length;
+                if (startLen == endLen)
+                {
+                    filteredRanges.Add((start, end));
+                }
+                else
+                {
+                    long rangeStart = start;
+                    for (int len = startLen; len < endLen; len++)
+                    {
+                        long rangeEnd = (long)(Math.Pow(10, len) - 1);
+                        filteredRanges.Add((rangeStart, rangeEnd));
+                        rangeStart = rangeEnd + 1;
+                    }
+                    filteredRanges.Add((rangeStart, end));
+                }
+            }
+
+            //iterate over filtered ranges
+            foreach (var (start, end) in filteredRanges)
+            {
+                var numberSet = new HashSet<long>();
+                //iterate through possible digit repeat patterns
+                int digitCount = start.ToString().Length; 
+
+                int upperLimit = isPart2 ? digitCount : 2;// for part 1 we only need to check pairs
+
+                for (int i = 2; i <= upperLimit; i++)
+                {
+                    if (digitCount % i != 0) continue; //skip if digit count not divisible by i
+
+                    //get fix start for the pattern
+                    long patternStart = 0;
+                    long patternEnd = 0;
+                    string patternStartString = "";
+                    string patternEndString = "";
+                    for (int j = 0; j <= (digitCount / i)-1; j++)
+                    {
+  
+                        if (start.ToString()[j] == end.ToString()[j])
+                        {
+                            patternStartString += start.ToString()[j];
+                            patternEndString += start.ToString()[j];
+                        }
+                        else
+                        {
+                            patternStartString += '0';
+                            patternEndString += '9';
+                        }                       
+                    }
+                    patternStart = long.Parse(patternStartString);
+                    patternEnd = long.Parse(patternEndString);
+
+                    for (long k = patternStart; k <= patternEnd; k++)
+                    {
+                        long num = 0;
+                        //construct candidate number by repeating i times the digit k
+                        string numString = string.Concat(Enumerable.Repeat(k.ToString(), i));
+                        num = long.Parse(numString);
+
+                        if (num >= start && num <= end)
+                        {
+                            numberSet.Add(num);
+                            //Console.WriteLine(num);
+                        }
+                    }
+                }
+
+                sum += numberSet.Sum();
+            }
+
+            return sum;
+        }
+
+        private static long Part1_BF_Regex(Day02_Input input)
+        {
             long sum = 0;
             foreach (var (start, end) in input)
             {
-                for(long i = start; i <= end; i++)
+                for (long i = start; i <= end; i++)
                 {
                     if (Regex.IsMatch(i.ToString(), @"^(\d+)\1$")) sum += i;
                 }
@@ -55,8 +147,14 @@ namespace AoC_2025
 
             return sum;
         }
-        
+
         public static long Day02_Part2(Day02_Input input)
+        {
+            //return Part2_BF_Regex(input);
+            return Optimized(input, true);
+        }
+
+        private static long Part2_BF_Regex(Day02_Input input)
         {
             long sum = 0;
             foreach (var (start, end) in input)
@@ -69,8 +167,6 @@ namespace AoC_2025
 
             return sum;
         }
-
-
     }
     public class Day02_Test
     {
