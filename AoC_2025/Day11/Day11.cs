343d615e1ca26bf8ca56864cdb3f7ab9b7092d65
Puskas David (MA-WS PAS-EDW) <pud2bp@bosch.com>
2025-12-11 09:03:18 +0100

    Day11 Part1 solution

--- AoC_2025/Day11/Day11.cs
+++ AoC_2025/Day11/Day11.cs
@@ -11,8 +11,9 @@ namespace AoC_2025
 {
     public static class Day11
     {
-        public class Day11_Input : List<string> //Define input type
+        public class Day11_Input : Dictionary<string, HashSet<string>> //Define input type
         {
+
         }
         public static void Day11_Main()
         {
@@ -32,7 +33,9 @@ namespace AoC_2025
 
             foreach (string line in rawinput.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).Select(s => s.Trim()))
             {
-                //result.Add(line);
+                var splitted1 = line.Split(':');
+                var destinations = splitted1[1].Trim().Split(' ').Select(s => s.Trim());
+                result.Add(splitted1[0].Trim(), new HashSet<string>(destinations));
             }
 
             return result;
@@ -42,12 +45,39 @@ namespace AoC_2025
         public static int Day11_Part1(Day11_Input input)
         {
 
-            return 0;
+            return Day11_RecursiveGetRoutesToOut(input, "you")["out"];
+        }
+
+        public static (Dictionary<string,int>, bool, bool) Day11_RecursiveGetRoutesToOut(Day11_Input input, string startPoint, bool isPart2 = false)
+        {
+            var start = input[startPoint];
+            var result = new Dictionary<string,int>();
+            var isfft = (startPoint=="fft");
+            var isdac = (startPoint=="dac");
+            foreach(var dest in start)
+            {
+                if (dest == "out")
+                {
+                    result.Add(dest, 1);
+                    continue;
+                }
+                var (subResult, subfft, subdac) = Day11_RecursiveGetRoutesToOut(input, dest);
+                foreach(var sub in subResult)
+                {
+                    if (subdac) isdac = true;
+                    if (subfft) isfft = true;
+                    if(!result.ContainsKey(sub.Key)) result.Add(sub.Key, sub.Value);
+                    else result[sub.Key] += sub.Value;
+                }
+   
+            }
+
+            return (result, isfft, isdac);
         }
 
         public static int Day11_Part2(Day11_Input input)
         {
-            return 0;
+            return Day11_RecursiveGetRoutesToOut(input, "srv", true)["out"];
         }
 
 
@@ -62,7 +92,7 @@ namespace AoC_2025
         }
 
         [Theory]
-        [InlineData("aaa: you hhh\r\nyou: bbb ccc\r\nbbb: ddd eee\r\nccc: ddd eee fff\r\nddd: ggg\r\neee: out\r\nfff: out\r\nggg: out\r\nhhh: ccc fff iii\r\niii: out", 0)]
+        [InlineData("svr: aaa bbb\r\naaa: fft\r\nfft: ccc\r\nbbb: tty\r\ntty: ccc\r\nccc: ddd eee\r\nddd: hub\r\nhub: fff\r\neee: dac\r\ndac: fff\r\nfff: ggg hhh\r\nggg: out\r\nhhh: out", 2)]
         public static void Day11Part2Test(string rawinput, int expectedValue)
         {
             Assert.Equal(expectedValue, Day11.Day11_Part2(Day11.Day11_ReadInput(rawinput)));
